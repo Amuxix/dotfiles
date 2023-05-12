2ef8483796258eadbc4acc594e920b6a6474c1b9
Amuxix <7691673+Amuxix@users.noreply.github.com>
2023-05-12 13:48:35 +0100

    Fix setup

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@ DOTFILES="$(pwd)"
 
 files=(".zshrc" ".p10k.zsh" ".config/nvim")
 
-for file in "$files[@]"
+for file in "${files[@]}"
 do
   f="$HOME/$file"
-  if [[ -d f ]] || [[ -f f ]] && ! [[ -L f ]]; then
+  if [[ -d $f ]] || [[ -f $f ]] && ! [[ -L $f ]]; then
     echo "$file already exists, move it to old/?"
-    mv -i f old/
+    if ! [[ -d "old" ]]; then
+      mkdir old
+    fi
+    mv -i $f old/
   fi
-  ln -df "$DOTFILES/$file"
+  ln -sf "$DOTFILES/$file" $f
 done
