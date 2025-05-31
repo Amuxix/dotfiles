8ca1f8e8c7c83e15b03d83e116c606edd6bd8c08
Sofia <7691673+Amuxix@users.noreply.github.com>
2025-05-31 20:54:18 +0100

    Improve setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,39 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Allow fingerprint for sudo in console
+sed "s/^#auth/auth/" /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local
+
+# Install rosetta
+sudo softwareupdate --install-rosetta
+
+# Install homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> "$HOME/.zprofile"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# CLI tools
+brew install git fzf direnv zoxide eza vimcat nvim scala sbt
+
+# GUI apps
+brew install --cask firefox intellij-idea enpass slack smooze-pro spotify alfred discord iterm2 font-fira-code-nerd-font
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+chsh -s $(which zsh)
+
+if git clone https://github.com/Amuxix/dotfiles.git; then
+  cd dotfiles
+else
+  echo "Failed to clone dotfiles repo."
+  exit 1
+fi
+
+cd dotfiles
 
 DOTFILES="$(pwd)"
 
@@ -16,3 +51,5 @@ do
   fi
   ln -sf "$DOTFILES/$file" $f
 done
+
+cd ..
