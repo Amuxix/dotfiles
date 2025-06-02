3f5298858a4123fcb81e967df51f227830d25628
Sofia <7691673+Amuxix@users.noreply.github.com>
2025-06-02 16:40:47 +0100

    use bash for homebrew

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ else
 fi
 
 # Install homebrew (does not require sudo)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo >> "$HOME/.zprofile"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
